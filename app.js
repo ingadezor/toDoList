9d183f5d5bab757fdc3bb73b79526f85e267127d
Inga <ingafezor@gmail.com>
2023-01-15 12:51:41 -0600

    add functionality to support work list type

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 let list=[];
+let workList = [];
+
 
 const app = express();
 app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public")); //letting know that our static files are in public folder
+
+
 
 
 //ROUTES
@@ -14,19 +19,32 @@ app.get('/', function(req, res){
     let options ={weekday: 'long', day: 'numeric', month: 'long'}
     let day = today.toLocaleDateString('en-US', options);
 
-    res.render('list', {day: day, list: list});
+    res.render('list', {title: day, list: list});
 
 })
 
 
+//work todolist
+app.get('/work', function(req, res){
+    res.render('list', {title: 'Work', list: workList});
+})
+
+
+
+
 //getting new listItem 
 app.post("/", function(req, res){
+    let listType = req.body.button;
     let listItem = req.body.listItem;
 
-    //adding item to list
-    list.push(listItem);
-
-    res.redirect('/');
+    if(listType == 'Work') {
+        workList.push(listItem);
+        res.redirect('/work');
+    }
+    else{
+        list.push(listItem);
+        res.redirect('/');
+    }
     
 })
 
