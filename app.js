390c64560928d36d8ef10f377f9caac62e4f8109
Inga <ingafezor@gmail.com>
2023-01-16 20:30:16 -0600

    create a todolistDB and adding default list items to Items collection using mongoose

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js"); //accessing my own module
+const mongoose = require('mongoose');
+
+//-------------------MONGOOSE 
+mongoose.connect("mongodb+srv://inga:inga@cluster0.lmpix.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true}, () => console.log('connected to mongo'));
+
+let itemSchema = new mongoose.Schema({
+    name: String
+})
+
+let Item = mongoose.model('Item', itemSchema); //model for Items collection
 
 
-let list=[];
-let workList = [];
+
+//setting default items and adding them to our db
+let item1 = new Item({
+    name: 'Welcome to your to do list!'
+});
+
+let item2 = new Item({
+    name: 'Hit + to add an item'
+});
+
+const defaultItems = [item1, item2];
+Item.insertMany(defaultItems, function(err){
+    if(err) console.log(err)
+    else console.log("Inserted default items in collection");
+})
+
+//---------------------------
+
+
+
+//let list=[];
+//let workList = [];
 
 
 const app = express();
@@ -17,7 +47,7 @@ app.use(express.static("public")); //letting know that our static files are in p
 
 //ROUTES
 app.get('/', function(req, res){
-    let day = date.getDate();
+    let day = date.getDate(); //using my owm module
 
     res.render('list', {title: day, list: list});
 
