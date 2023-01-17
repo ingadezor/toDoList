054d9a7d179bd9561e16362a31d083d9eb8151c5
Inga <ingafezor@gmail.com>
2023-01-16 21:10:42 -0600

    add the process of extracting/adding items from/to mongoDB in routes of the app

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js"); //accessing my own module
 const mongoose = require('mongoose');
 
-//-------------------MONGOOSE 
+
+
+//-------------------CONNECTION TO MONGOOSE AND ADDING DEFAULT LIST ITEMS TO COLLECTION 
 mongoose.connect("mongodb+srv://inga:inga@cluster0.lmpix.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true}, () => console.log('connected to mongo'));
 
 let itemSchema = new mongoose.Schema({
     name: String
 })
-
 let Item = mongoose.model('Item', itemSchema); //model for Items collection
+let WorkItem = mongoose.model('WorkItem', itemSchema);
+
 
 
 
@@ -20,21 +23,14 @@ let item1 = new Item({
 });
 
 let item2 = new Item({
-    name: 'Hit + to add an item'
+    name: 'Hit  +  to add an item'
 });
 
+//default items are only included in the main list (not work one)
 const defaultItems = [item1, item2];
-Item.insertMany(defaultItems, function(err){
-    if(err) console.log(err)
-    else console.log("Inserted default items in collection");
-})
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
-//---------------------------
-
-
-
-//let list=[];
-//let workList = [];
 
 
 const app = express();
@@ -49,14 +45,28 @@ app.use(express.static("public")); //letting know that our static files are in p
 app.get('/', function(req, res){
     let day = date.getDate(); //using my owm module
 
-    res.render('list', {title: day, list: list});
+    Item.find(function(err, items){ //items is an array of all objects in items collection
+        //if no items initially in the items collection -> add default items and render list 
+        if(items.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err) console.log(err)
+                else console.log("Inserted default items in collection");
+            })
 
+            res.redirect('/');
+        }
+        else res.render('list', {title: day, list: items});
+    }) 
 })
 
 
 //work todolist
 app.get('/work', function(req, res){
-    res.render('list', {title: 'Work', list: workList});
+
+    WorkItem.find(function(err, workItems){
+        res.render('list', {title: 'Work', list: workItems});
+    })
+    
 })
 
 
@@ -67,12 +77,26 @@ app.post("/", function(req, res){
     let listType = req.body.button;
     let listItem = req.body.listItem;
 
+
+
     if(listType == 'Work') {
-        workList.push(listItem);
+        //workList.push(listItem);
+        let item = new WorkItem({
+            name: listItem
+        })
+        item.save();
+
+
         res.redirect('/work');
     }
     else{
-        list.push(listItem);
+        //list.push(listItem);
+        //adding item to main list
+        let item = new Item({
+            name: listItem
+        })
+        item.save();
+
         res.redirect('/');
     }
     
